936b5b942d6d2b2162b3f08d467d79603ecbf588
Jiaqi Zhang <117554189+jiaqi-fmc@users.noreply.github.com>
2023-05-24 12:20:06 +0800

    ClariSpend installation scripts (#2)

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# FormaCloud
+# FormaCloud Installation Guide
+
+Cloud Savings Simplified.
+
+## Supported Platforms
+
+Linux/Unix
+
+## Prerequisites
+
+1. [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html) installed.
+2. [jq](https://stedolan.github.io/jq/download/) installed.
+3. AWS credentials of your root account set:
+
+```bash
+export AWS_ACCESS_KEY_ID=xxx
+export AWS_SECRET_ACCESS_KEY=xxx
+export AWS_SESSION_TOKEN=xxx
+```
+
+## ClariSpend
+
+1. Contact FormaCloud support to get the following environment variables:
+
+```bash
+export FormaCloudID=xxx  # The customer ID that syncs your account.
+export FormaCloudPrincipal=xxx  # The IAM Principal that has permission to your account.
+export FormaCloudExternalID=xxx  # The external ID that authenticates your account.
+export FormaCloudPingbackArn=xxx  # The custom resource to receive pingback.
+export FormaCloudService=xxx  # The FormaCloud service type.
+```
+
+2. To install ClariSpend, run the following command:
+
+```bash
+$ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/forma-cloud/FormaCloud/main/clarispend/install.sh)"
+```
+
+Sample output:
+
+```
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100  3047  100  3047    0     0   3210      0 --:--:-- --:--:-- --:--:--  3217
+Creating a Stack...
+...
+Stack created!
+Creating a StackSet...
+...
+FormaCloudClariSpend StackSet created!
+Creating StackSet instances for the member accounts...
+...
+Waiting for the above operation to finish..................
+Operation finished:
+FormaCloudClariSpend StackSet instances created!
+```
+
+3. To uninstall ClariSpend, run the following command:
+
+```bash
+$ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/forma-cloud/FormaCloud/main/clarispend/uninstall.sh)"
+```
+
+Sample output:
+
+```
+Deleting the StackSet instances for the member accounts...
+...
+Waiting for the above operation to finish...
+Operation finished:
+...
+FormaCloudClariSpend StackSet instances deleted!
+Deleting the StackSet...
+FormaCloudClariSpend StackSet Deleted!
+Deleting the Stack...
+FormaCloudClariSpend Stack deleted!
+```
---
+++ clarispend/install.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+
+function stackSetOperationWait {
+  local cmd="aws cloudformation describe-stack-set-operation --stack-set-name ${1?} --operation-id ${2?}"
+  $cmd
+  printf "Waiting for the above operation to finish..."
+  while true; do
+    sleep 1
+    local end_timestamp="$($cmd --query "StackSetOperation.EndTimestamp" --output text)"
+    if [ "${end_timestamp}" != "None" ]; then
+      printf "\nOperation finished:\n"
+      break
+    fi
+    printf '.'
+  done
+  $cmd
+  local status="$($cmd --query "StackSetOperation.Status" --output text)"
+  if [ "${status?}" != "SUCCEEDED" ]; then
+    echo "StackSet operation did not succeed. Stack instances:"
+    aws cloudformation list-stack-instances --stack-set-name "${1?}"
+    exit 1
+  fi
+}
+
+stack_name=FormaCloudClariSpend
+root_account_id=$(aws organizations describe-organization | jq -r .Organization.MasterAccountId)
+main_region=$(aws configure get region)
+org_id=$(aws organizations list-roots | jq -r .Roots[0].Id)
+
+test -n "$FORMACLOUD_ID" || die "FORMACLOUD_ID must be provided. Please contact FormaCloud support.";
+test -n "$FORMACLOUD_PRINCIPAL" || die "FORMACLOUD_PRINCIPAL must be provided. Please contact FormaCloud support.";
+test -n "$FORMACLOUD_EXTERNALID" || die "FORMACLOUD_EXTERNALID must be provided. Please contact FormaCloud support.";
+test -n "$FORMACLOUD_PINGBACK_ARN" || die "FORMACLOUD_PINGBACK_ARN must be provided. Please contact FormaCloud support.";
+test -n "$root_account_id" || die "AWS root account id not found.";
+test -n "$main_region" || die "AWS default region not found.";
+test -n "$org_id" || die "AWS root organization id not found.";
+
+tmp_dir=$(mktemp -d)
+tmp_file=${tmp_dir}/formacloud_clarispend.yaml
+curl -o ${tmp_file} https://raw.githubusercontent.com/forma-cloud/FormaCloud/main/clarispend/formacloud_clarispend.yaml
+
+echo "Creating a Stack..."
+aws cloudformation create-stack \
+--stack-name ${stack_name} \
+--capabilities CAPABILITY_NAMED_IAM \
+--template-body file://${tmp_file} \
+--parameters ParameterKey=FormaCloudID,ParameterValue=${FORMACLOUD_ID} \
+ParameterKey=FormaCloudPrincipal,ParameterValue=${FORMACLOUD_PRINCIPAL} \
+ParameterKey=FormaCloudExternalID,ParameterValue=${FORMACLOUD_EXTERNALID} \
+ParameterKey=FormaCloudPingbackArn,ParameterValue=${FORMACLOUD_PINGBACK_ARN} \
+ParameterKey=FormaCloudService,ParameterValue=${FORMACLOUD_SERVICE} \
+ParameterKey=RootAccountID,ParameterValue=${root_account_id}
+echo "Stack created!"
+
+echo "Creating a StackSet..."
+aws cloudformation create-stack-set \
+--stack-set-name ${stack_name} \
+--capabilities CAPABILITY_NAMED_IAM \
+--auto-deployment Enabled=true,RetainStacksOnAccountRemoval=true \
+--permission-mode SERVICE_MANAGED \
+--template-body file://${tmp_file} \
+--parameters ParameterKey=FormaCloudID,ParameterValue=${FORMACLOUD_ID} \
+ParameterKey=FormaCloudPrincipal,ParameterValue=${FORMACLOUD_PRINCIPAL} \
+ParameterKey=FormaCloudExternalID,ParameterValue=${FORMACLOUD_EXTERNALID} \
+ParameterKey=FormaCloudPingbackArn,ParameterValue=${FORMACLOUD_PINGBACK_ARN} \
+ParameterKey=FormaCloudService,ParameterValue=${FORMACLOUD_SERVICE} \
+ParameterKey=RootAccountID,ParameterValue=${root_account_id}
+echo "${stack_name} StackSet created!"
+
+echo "Creating StackSet instances for the member accounts..."
+operation_id="$(aws cloudformation create-stack-instances \
+--stack-set-name ${stack_name} \
+--regions ${main_region} \
+--deployment-targets OrganizationalUnitIds=${org_id} \
+--operation-preferences RegionConcurrencyType=PARALLEL,MaxConcurrentPercentage=100,FailureTolerancePercentage=100 \
+--output text)"
+stackSetOperationWait ${stack_name} "${operation_id}"
+echo "${stack_name} StackSet instances created!"
+
+rm -r ${tmp_dir}
