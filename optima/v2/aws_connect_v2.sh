f2377530c57451787b4cbd017989b7f2a9b3691c
jiaqi-fmc <jiaqi@formacloud.io>
2023-09-15 12:34:29 +0800

    Revert "SavingBot connect script"
    This reverts commit f0ef09d065f856b259303d4fecf59c130dc60759.

--- optima/v2/aws_connect_v2.sh
+++ optima/v2/aws_connect_v2.sh
@@ -27,15 +27,13 @@ function stackSetOperationWait {
   fi
 }
 
-STACK_NAME=FormaCloudSavingBot
-TEMPLATE_URL=https://formacloud-public.s3.us-west-2.amazonaws.com/formacloud-savingbot-latest.json
-
 FORMACLOUD_ID=""
-FORMACLOUD_PRINCIPAL=""
 FORMACLOUD_EXTERNALID=""
-MAIN_REGION=""
 REGIONS=()
+FULL_ORGANIZATION=false
 CW_ROLE_EXISTS=false
+FORMACLOUD_PRINCIPAL=872291417442
+FORMACLOUD_EVENT_BUS_ARN=arn:aws:events:us-west-2:872291417442:event-bus/savingbot-event-bus
 
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -43,16 +41,8 @@ while [[ $# -gt 0 ]]; do
             FORMACLOUD_ID="$2"
             shift 2
             ;;
-        -p)
-            FORMACLOUD_PRINCIPAL="$2"
-            shift 2
-            ;;
         -e)
             FORMACLOUD_EXTERNALID="$2"
-            shift 2
-            ;;
-        -m)
-            MAIN_REGION="$2"
             shift 2
             ;;
         -r)
@@ -61,6 +51,10 @@ while [[ $# -gt 0 ]]; do
                 REGIONS+=("$1")
                 shift
             done
+            ;;
+        -o)
+            FULL_ORGANIZATION=true
+            shift
             ;;
         -c)
             CW_ROLE_EXISTS=true
@@ -75,64 +69,81 @@ done
 
 test -n "$FORMACLOUD_ID" || die "FORMACLOUD_ID must be provided. Please contact FormaCloud support."
 test -n "$FORMACLOUD_EXTERNALID" || die "FORMACLOUD_EXTERNALID must be provided. Please contact FormaCloud support."
-test -n "$MAIN_REGION" || die "MAIN_REGION must be provided. Please contact FormaCloud support."
 test -n "$REGIONS" || die "REGIONS must be provided. e.g. us-west-2 us-east-1"
 
+main_region=${REGIONS[0]}
 regions_str=$(IFS=";"; echo "${REGIONS[*]}")
-formacloud_pingback_arn=arn:aws:sns:${MAIN_REGION}:${FORMACLOUD_PRINCIPAL}:formacloud-pingback-topic
-formacloud_event_bus_arn=arn:aws:events:${MAIN_REGION}:${FORMACLOUD_PRINCIPAL}:event-bus/savingbot-event-bus
+formacloud_pingback_arn=arn:aws:sns:${main_region}:${FORMACLOUD_PRINCIPAL}:formacloud-pingback-topic
 
-root_account_id=$(aws sts get-caller-identity --query "Account" --output text)
-org_id=$(aws organizations list-roots --query "Roots[0].Id" --output text)
+if $FULL_ORGANIZATION; then
+  root_account_id=$(aws organizations describe-organization | jq -r .Organization.MasterAccountId)
+else
+  root_account_id=$(aws sts get-caller-identity --query "Account" --output text)
+fi
+
+tmp_dir=$(mktemp -d)
+tmp_file="$tmp_dir"/formacloud_optima.yaml
+curl -fsSL -o "$tmp_file" https://raw.githubusercontent.com/forma-cloud/FormaCloud/main/optima/v2/formacloud_optima_v2.yaml
+
+stack_name=FormaCloudOptima
 
 for region in "${REGIONS[@]}"; do
   echo "Creating a Stack in ${region}..."
   aws cloudformation create-stack \
   --region ${region} \
-  --stack-name ${STACK_NAME} \
+  --stack-name ${stack_name} \
   --capabilities CAPABILITY_NAMED_IAM \
-  --template-url ${TEMPLATE_URL} \
+  --template-body file://${tmp_file} \
   --parameters ParameterKey=FormaCloudID,ParameterValue=${FORMACLOUD_ID} \
   ParameterKey=FormaCloudPrincipal,ParameterValue=${FORMACLOUD_PRINCIPAL} \
   ParameterKey=FormaCloudExternalID,ParameterValue=${FORMACLOUD_EXTERNALID} \
-  ParameterKey=FormaCloudEventBusArn,ParameterValue=${formacloud_event_bus_arn} \
+  ParameterKey=FormaCloudEventBusArn,ParameterValue=${FORMACLOUD_EVENT_BUS_ARN} \
   ParameterKey=CWCrossAccountSharingRoleExists,ParameterValue=${CW_ROLE_EXISTS} \
-  ParameterKey=MainRegion,ParameterValue=${MAIN_REGION} \
+  ParameterKey=MainRegion,ParameterValue=${main_region} \
   ParameterKey=Regions,ParameterValue=${regions_str} \
   ParameterKey=RootAccountID,ParameterValue=${root_account_id} \
   ParameterKey=FormaCloudPingbackArn,ParameterValue=${formacloud_pingback_arn}
 done
-echo "${STACK_NAME} Stacks created!"
+echo "${stack_name} Stacks created!"
+
+if ! $FULL_ORGANIZATION; then
+  echo "Enabling compute optimizer..."
+  aws compute-optimizer update-enrollment-status --status Active
+  echo "Connection completed."
+  exit 0
+fi
+
+org_id=$(aws organizations list-roots | jq -r .Roots[0].Id)
 
 echo "Creating a StackSet..."
 aws cloudformation create-stack-set \
---region ${MAIN_REGION} \
---stack-set-name ${STACK_NAME} \
+--region ${main_region} \
+--stack-set-name ${stack_name} \
 --capabilities CAPABILITY_NAMED_IAM \
 --auto-deployment Enabled=true,RetainStacksOnAccountRemoval=true \
 --permission-mode SERVICE_MANAGED \
---template-url ${TEMPLATE_URL} \
+--template-body file://${tmp_file} \
 --parameters ParameterKey=FormaCloudID,ParameterValue=${FORMACLOUD_ID} \
 ParameterKey=FormaCloudPrincipal,ParameterValue=${FORMACLOUD_PRINCIPAL} \
 ParameterKey=FormaCloudExternalID,ParameterValue=${FORMACLOUD_EXTERNALID} \
-ParameterKey=FormaCloudEventBusArn,ParameterValue=${formacloud_event_bus_arn} \
+ParameterKey=FormaCloudEventBusArn,ParameterValue=${FORMACLOUD_EVENT_BUS_ARN} \
 ParameterKey=CWCrossAccountSharingRoleExists,ParameterValue=${CW_ROLE_EXISTS} \
 ParameterKey=RootAccountID,ParameterValue=${root_account_id} \
-ParameterKey=MainRegion,ParameterValue=${MAIN_REGION} \
+ParameterKey=MainRegion,ParameterValue=${main_region} \
 ParameterKey=Regions,ParameterValue=${regions_str} \
 ParameterKey=FormaCloudPingbackArn,ParameterValue=${formacloud_pingback_arn}
-echo "${STACK_NAME} StackSet created!"
+echo "${stack_name} StackSet created!"
 
 echo "Creating StackSet instances for the member accounts..."
 operation_id="$(aws cloudformation create-stack-instances \
---region ${MAIN_REGION} \
---stack-set-name ${STACK_NAME} \
+--region ${main_region} \
+--stack-set-name ${stack_name} \
 --regions ${REGIONS[*]} \
 --deployment-targets OrganizationalUnitIds=${org_id} \
 --operation-preferences RegionConcurrencyType=PARALLEL,MaxConcurrentPercentage=100,FailureTolerancePercentage=100 \
 --output text)"
-stackSetOperationWait "$MAIN_REGION" "$STACK_NAME" "$operation_id"
-echo "${STACK_NAME} StackSet instances created!"
+stackSetOperationWait "$main_region" "$stack_name" "$operation_id"
+echo "${stack_name} StackSet instances created!"
 
 echo "Enabling compute optimizer for the organization..."
 aws compute-optimizer update-enrollment-status --status Active --include-member-accounts
