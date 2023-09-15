f15395e54fd4c271b634f6299f6e9679b40e5fb1
Jiaqi Zhang <117554189+jiaqi-fmc@users.noreply.github.com>
2023-09-16 00:49:51 +0800

    Autopilot connect/remove scripts for member accounts (#33)

---
+++ optima/autopilot/aws-autopilot-remove.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+
+function stackSetOperationWait {
+  local cmd="aws cloudformation describe-stack-set-operation --region ${1?} --stack-set-name ${2?} --operation-id ${3?}"
+  $cmd
+  printf "Waiting for the above operation to finish..."
+  while true; do
+    sleep 5
+    local end_timestamp="$($cmd --query "StackSetOperation.EndTimestamp" --output text)"
+    if [ "${end_timestamp}" != "None" ]; then
+      printf '\nOperation finished:\n'
+      break
+    fi
+    printf '.'
+  done
+  $cmd
+  local status="$($cmd --query "StackSetOperation.Status" --output text)"
+  if [ "${status?}" != "SUCCEEDED" ]; then
+    echo "StackSet operation did not succeed. Stack instances:"
+    aws cloudformation list-stack-instances --stack-set-name "${1?}"
+  fi
+}
+
+MAIN_REGION=us-west-2
+STACK_NAME=FormaCloudAutopilot
+
+org_id=$(aws organizations list-roots --query "Roots[0].Id" --output text)
+
+echo "Deleting the StackSet instances for the member accounts..."
+operation_id="$(aws cloudformation delete-stack-instances \
+--region ${MAIN_REGION} \
+--stack-set-name ${STACK_NAME} \
+--regions ${MAIN_REGION} \
+--no-retain-stacks \
+--deployment-targets OrganizationalUnitIds=${org_id} \
+--operation-preferences RegionConcurrencyType=PARALLEL,MaxConcurrentPercentage=100,FailureTolerancePercentage=100 \
+--output text)"
+stackSetOperationWait ${MAIN_REGION} ${STACK_NAME} ${operation_id}
+echo "${STACK_NAME} StackSet instances deleted!"
+
+echo "Deleting the StackSet..."
+aws cloudformation delete-stack-set \
+--region ${MAIN_REGION} \
+--stack-set-name ${STACK_NAME}
+echo "${STACK_NAME} StackSet deleted!"
+
+echo "Removal completed."
